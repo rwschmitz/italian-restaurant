407f34d2c8b078c584ed2772fc208b668f6aca52
DESKTOP-IO8IAI6\Rudol <rudolph.w.schmitz@gmail.com>
2018-02-24 17:53:37 -0600

    Fix logo becoming too big when mobile menu is opened

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector('.nav-menu-icon');
 const nav = document.querySelector('.nav');
 const navMenu = document.querySelector('.nav-menu');
+const navImage = document.querySelector('.nav img');
 
 function toggleMenu () {
   navMenu.classList.toggle('is-active');
